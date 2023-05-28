f2612f640d835c974beb746d16fbf6d895e1dc63
Lavander40 <a.kirill.03@mail.ru>
2023-05-29 01:51:50 +0300

    urlconnection done

--- httpurlconnection/src/main/java/ru/mirea/astaevka/httpurlconnection/MainActivity.java
+++ httpurlconnection/src/main/java/ru/mirea/astaevka/httpurlconnection/MainActivity.java
@@ -2,6 +2,7 @@ package ru.mirea.astaevka.httpurlconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -24,6 +25,7 @@ import ru.mirea.astaevka.httpurlconnection.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,48 +41,107 @@ public class MainActivity extends AppCompatActivity {
                     networkinfo = connectivityManager.getActiveNetworkInfo();
                 }
                 if (networkinfo != null && networkinfo.isConnected()) {
+
                     new DownloadPageTask().execute("https://ipinfo.io/json"); // запуск нового потока
 
                 } else {
-                    Toast.makeText(this, "Нет интернета", Toast.LENGTH_SHORT).show();
+                    binding.textView.setText("Нет интернета");
+                }
+            }
+        });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConnectivityManager connectivityManager =
+                        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkinfo = null;
+                if (connectivityManager != null) {
+                    networkinfo = connectivityManager.getActiveNetworkInfo();
+                }
+                if (networkinfo != null && networkinfo.isConnected()) {
+
+                    new DownloadPageTaskWeather().execute("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"); // запуск нового потока
+
+                } else {
+                    binding.textView.setText("Нет интернета");
                 }
             }
         });
     }
-}
 
- class DownloadPageTask extends AsyncTask<String, Void, String> {
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        binding.textView.setText("Загружаем...");
-    }
-
-    @Override
-    protected String doInBackground(String... urls) {
-        try {
-            return downloadIpInfo(urls[0]);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "error";
+    private class DownloadPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            binding.textView.setText("Загружаем...");
+            binding.textView2.setText("Загружаем...");
+            binding.textView3.setText("Загружаем...");
+            binding.textView4.setText("Загружаем...");
+        }
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadIpInfo(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(MainActivity.class.getSimpleName(), result);
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                Log.d(MainActivity.class.getSimpleName(), "Response: " + responseJson);
+                String ip = responseJson.getString("ip");
+                Log.d(MainActivity.class.getSimpleName(), "IP: " + ip);
+                binding.textView.setText("Город: " + responseJson.getString("city"));
+                binding.textView2.setText("Регион: " +responseJson.getString("region"));
+                binding.textView3.setText("Ip: " + responseJson.getString("ip"));
+                binding.textView4.setText("Координаты: " + responseJson.getString("loc"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(result);
         }
     }
 
-    @Override
-    protected void onPostExecute(String result) {
-        //binding.textView.setText(result);
-        Log.d(MainActivity.class.getSimpleName(), result);
-        try {
-            JSONObject responseJson = new JSONObject(result);
-            Log.d(MainActivity.class.getSimpleName(), "Response: " + responseJson);
-            String ip = responseJson.getString("ip");
-            Log.d(MainActivity.class.getSimpleName(), "IP: " + ip);
-        } catch (JSONException e) {
-            e.printStackTrace();
+    private class DownloadPageTaskWeather extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            binding.textView5.setText("Загружаем...");
+            binding.textView6.setText("Загружаем...");
+            binding.textView7.setText("Загружаем...");
+            binding.textView8.setText("Загружаем...");
         }
-        super.onPostExecute(result);
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadIpInfo(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(MainActivity.class.getSimpleName(), result);
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                Log.d(MainActivity.class.getSimpleName(), "Response: " + responseJson);
+                JSONObject weatherJSON = responseJson.getJSONObject("current_weather");
+                binding.textView5.setText("Температура: " + weatherJSON.getString("temperature"));
+                binding.textView6.setText("Скорость ветра: " + weatherJSON.getString("windspeed"));
+                binding.textView7.setText("Направление ветра: " + weatherJSON.getString("winddirection"));
+                binding.textView8.setText("Время: " + weatherJSON.getString("time"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(result);
+        }
     }
-
     private String downloadIpInfo(String address) throws IOException {
         InputStream inputStream = null;
         String data = "";
@@ -99,12 +160,11 @@ public class MainActivity extends AppCompatActivity {
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 int read = 0;
                 while ((read = inputStream.read()) != -1) {
-                    bos.write(read);
-                }
+                    bos.write(read); }
                 bos.close();
                 data = bos.toString();
             } else {
-                data = connection.getResponseMessage() + ". Error Code: " + responseCode;
+                data = connection.getResponseMessage()+". Error Code: " + responseCode;
             }
             connection.disconnect();
         } catch (IOException e) {
